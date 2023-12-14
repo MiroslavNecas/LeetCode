8ee695a27481838c382f3b862cc0848894f56344
Miroslav Nečas <miras.necas@seznam.cz>
2023-12-14 21:34:30 +0100

    7th problem

---
+++ 6. ZigZag Conversion/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime;
+
+namespace _6._ZigZag_Conversion
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Solution solution = new Solution();
+			solution.Convert("ABC", 2);
+		}
+	}
+	public class Solution
+	{
+		public string Convert(string s, int numRows)
+		{
+			return "abc";
+		}
+	}
+}
---
+++ 7. Reverse Integer/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _7._Reverse_Integer
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Solution solution = new Solution();
+			solution.Reverse(1534236469);
+		}
+	}
+
+	public class Solution
+	{
+		public int Reverse(int x)
+		{
+			int reversed = 0;
+			while (x!=0) 
+			{
+				if (reversed > int.MaxValue / 10 || reversed < int.MinValue / 10) return 0;
+				reversed = reversed * 10 + x % 10;
+				x = x / 10;
+			}
+			return reversed;
+		}
+	}
+}

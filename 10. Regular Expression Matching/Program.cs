99acd11cf8cbf03567a69f561297382fc37fc5b6
Miroslav Nečas <miras.necas@seznam.cz>
2023-12-15 12:05:13 +0100

    redon problem number 3

---
+++ 10. Regular Expression Matching/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _10._Regular_Expression_Matching
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+		}
+	}
+	public class Solution
+	{
+		public bool IsMatch(string s, string p)
+		{
+
+		}
+	}
+}

4bd01551289e19686437954a39743c74ee8776e6
Miroslav Nečas <miras.necas@seznam.cz>
2023-12-15 11:00:27 +0100

    9th problem

---
+++ 9. Palindrome Number/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _9._Palindrome_Number
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Solution solution = new Solution();
+			bool answer = solution.IsPalindrome(11221);
+			Console.WriteLine(answer);
+		}
+	}
+	public class Solution
+	{
+		public bool IsPalindrome(int x)
+		{
+			if (x < 0 ) return false;
+
+			string strX = x.ToString();
+
+			for (int i = 0; i < strX.Length / 2; i++)
+			{
+				if (strX[i] != strX[strX.Length - 1 - i]) return false;
+			}
+
+			return true;
+		}
+	}
+}

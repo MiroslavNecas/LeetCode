5317caf14a8e271b14e19e249156d3d1627f369c
Miroslav Nečas <miras.necas@seznam.cz>
2023-12-15 14:11:33 +0100

    adds problem number 6, i may come to this later. This solution is kinda bad.

--- 6. ZigZag Conversion/Program.cs
+++ 6. ZigZag Conversion/Program.cs
@@ -7,14 +7,42 @@ namespace _6._ZigZag_Conversion
 		static void Main(string[] args)
 		{
 			Solution solution = new Solution();
-			solution.Convert("ABC", 2);
+			string answer = solution.Convert("AB", 1);
+			Console.WriteLine(answer);
 		}
 	}
 	public class Solution
 	{
 		public string Convert(string s, int numRows)
 		{
-			return "abc";
+			if (s.Length == 0) return "";
+			if (s.Length <= numRows || numRows ==1) return s;
+			int y = 0;
+			bool down = true;
+
+			List<List<char>> listOfLists = new List<List<char>>();
+
+			for(int j = 0; j < numRows;j++)
+				listOfLists.Add(new List<char>());
+
+			for (int i = 0; i < s.Length; i++) 
+			{
+				listOfLists[y].Add(s[i]);
+				if (down) y++;
+				else y--;
+
+				if (y == numRows-1) down = false;
+				if (y == 0) down = true;
+			}
+			string answer = "";
+			foreach (List<char> sublist in listOfLists)
+			{
+				foreach (char c in sublist)
+				{
+					answer += c;
+				}
+			}
+			return answer;
 		}
 	}
 }

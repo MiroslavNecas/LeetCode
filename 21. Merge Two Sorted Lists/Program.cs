e670a6d115c3367120d6f2b21b9f23306428ed48
MiroslavNecas <miras.necas@seznam.cz>
2023-12-20 00:10:09 +0100

    added couple solutions

---
+++ 21. Merge Two Sorted Lists/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace _21._Merge_Two_Sorted_Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+            ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+
+            Solution solution = new Solution();
+            //solution.PrintLinkedList(list1);
+            //solution.PrintLinkedList(list2);
+            ListNode list = solution.MergeTwoLists(list1, list2);
+            solution.PrintLinkedList(list);
+        }
+    }
+
+    public class Solution
+    {
+        public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+            ListNode dummy = new ListNode();
+            ListNode current = dummy;
+
+            while (list1 != null && list2 != null)
+            {
+                if (list1.val < list2.val)
+                {
+                    current.next = list1;
+                    list1 = list1.next;
+                }
+                else
+                {
+                    current.next = list2;
+                    list2 = list2.next;
+                }
+                current = current.next;
+            }
+
+            if (list1 != null)
+            {
+                current.next = list1;
+            }
+            else if (list2 != null)
+            {
+                current.next = list2;
+            }
+
+            return dummy.next;
+        }
+        public void PrintLinkedList(ListNode head)
+        {
+            ListNode current = head;
+            while (current != null)
+            {
+                Console.Write($"{current.val} -> ");
+                current = current.next;
+            }
+            Console.Write("null");
+        }
+    }
+
+
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+}
---
+++ 26. Remove Duplicates from Sorted Array/Program.cs
@@ -0,0 +1,33 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace _26._Remove_Duplicates_from_Sorted_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int answer = solution.RemoveDuplicates(new int[] { 1,2,3 });
+            Console.WriteLine("asnwer is: "+answer);
+        }
+    }
+
+    public class Solution
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            HashSet<int> visited = new HashSet<int>();
+            int index = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (!visited.Contains(nums[i]))
+                {
+                    visited.Add(nums[i]);
+                    nums[index] = nums[i];
+                    index++;
+                }
+            }
+            return index;
+        }
+    }
+}
